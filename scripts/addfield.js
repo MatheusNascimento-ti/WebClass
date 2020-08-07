3f94660e8ec3dae90c415eda882acc3e1f901483
MatheusNascimento-ti <69018184+MatheusNascimento-ti@users.noreply.github.com>
2020-08-07 03:43:27 -0300

    first script created
    making the add time button work using javascript

---
+++ scripts/addfield.js
@@ -0,0 +1,14 @@
+document.querySelector("#add-time").addEventListener('click', clonefield)
+
+function clonefield(){
+
+    const newfieldcontainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    const fields = newfieldcontainer.querySelectorAll('input')
+
+    fields.forEach(function(fields){
+        fields.value = " "
+    })
+
+    document.querySelector('#schedule-items').appendChild(newfieldcontainer)
+}   
